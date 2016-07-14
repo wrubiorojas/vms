4c604cb0d95d7440832e0010e6c6aca0f6c75e2e
wilmer rubio <wrubiorojas@gmail.com>
2016-07-14 10:05:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var vmsApp = angular.module("vmSignage", ["ngRoute", "ngResource"]);
+var vmsApp = angular.module("vmSignage", ['ngRoute', 'ngResource']);
+
+//
+// Consume Json with user data
+//
+vmsApp.service("dataUser", ["$resource", function($resource) {
+
+    var getUserInformation = $resource("data/data.json", {}, {
+        query: {
+            method: "GET",
+            isArray: false
+        }
+    });
+
+    return getUserInformation;
+
+}]);
