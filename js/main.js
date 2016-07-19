9aa9ec529817b5947491906b75477be8388c6c51
wilmer rubio <wrubiorojas@gmail.com>
2016-07-19 11:10:51 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+require.config({
+    baseUrl: "js",
+    paths: {
+        'angular': 'vendor/angular/angular.min.js',
+        'angular-route': 'vendor/angular/angular-route.min',
+        'angular-resource': 'vendor/angular/angular-resource.min',
+        'angularAMD': 'vendor/angularAMD/angularAMD.min'
+    },
+    shim: { 'angularAMD': ['angular'], 'angular-route': ['angular'], 'angular-resource': ['angular'] },
+    deps: ['app']
+});
+
+
+
+
 var vmsApp = angular.module("vmSignage", ['ngRoute', 'ngResource']);
 
 // =======================================================================================
